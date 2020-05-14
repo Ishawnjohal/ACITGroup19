19ad33cdbacaa9a5a1f6de354386c1e2d1e82740
Roy Ortega <royeo91@gmail.com>
2020-05-14 11:58:33 -0700

    remove sleep

--- test/selenium_test.py
+++ test/selenium_test.py
@@ -106,7 +106,6 @@ class TestWebsite(unittest.TestCase):
 
         
         self.driver.find_element_by_id('login-button').click()
-        time.sleep(10)
         
         login_area = self.driver.find_element_by_id('username-257')
         password_area = self.driver.find_element_by_id('user_password-257')
